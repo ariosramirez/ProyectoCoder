b0c9b3d79dcf05be0cc521438c6082d19ac373e2
Andres Rios <ariosramirez.data@gmail.com>
2023-04-27 15:09:30 -0300

    added Tests

--- AppCoder/tests.py
+++ AppCoder/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from models import Profesor
+
+
+
+class RegistrarTestCase(TestCase):
+    def setUp(self):
+       self.profesor = Profesor.objects.create(nombre="Juan", apellido="Perez", email="juan@gmail.com", profesion="Ingeniero")
+       self.url = reverse('EliminarProfesor', args=[self.profesor.nombre])
+       
+    def test_eliminar_profesor(self):
+        respuesta = self.client.get(self.url)
+        self.assertEqual(respuesta.status_code, 200)
+        
+        self.assertTemplateUsed(respuesta, 'AppCoder/leerProfesores.html')
+        self.assertQuerysetEqual(Profesor.objects.all(), [])
+
+ 
